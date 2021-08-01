dc467c256d5a62429aa4d6b4719163dff7a460cd
Daniele Galvão <56988997+DanyJesus@users.noreply.github.com>
2021-08-01 05:31:42 -0300

    Projeto Vue
    Projeto completo

---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-col >
+      <b-card
+      :title="nome"
+        header="Primary"       
+        img-src="https://picsum.photos/id/237/200/300"
+        img-alt="Curso de Vue.JS"
+        img-top
+        tag="article"
+        style="max-width:20rem;"
+        class="mb-5"
+        border-variant="primary"
+        header-bg-variant="primary"
+        header-text-variant="white"
+        align="center"
+         >
+        <b-card-text>"Honra teu pai e tua mãe, a fim de que tenhas vida longa na terra que o Senhor,o teu Deus, te dá.</b-card-text>
+        <b-button variant="primary">Comprar</b-button>
+      </b-card>
+    </b-col>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            required: true,
+        }
+    },
+
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+      
+    <b-container>
+   
+    <b-row align-v="center">
+
+         <card v-for="curso in cursos" :key="curso.id" :nome="curso.nome"></card>  
+         
+    </b-row>    
+  </b-container>
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '@/components/Card.vue';
+
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      cursos: []
+    }
+  },
   components: {
-    HelloWorld
-  }
+    Card,
+  },
+  mounted() {
+      this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      const res = await fetch("cursos.json");
+      const val = await res.json();
+      this.cursos = val;
+      console.log(val);
+    }
+  },
 }
 </script>
